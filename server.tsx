cd0884c6a78795138e3f1e2314e0ca4066d1e41f
Jonny Burger <jonathanburger11@gmail.com>
2021-02-21 15:36:14 +0100

    fix type error for now¨

--- server.tsx
+++ server.tsx
@@ -35,7 +35,10 @@ app.get('/', async (req, res) => {
 			sendFile(cache.get(JSON.stringify(req.query)) as string);
 			return;
 		}
-		const bundled = await bundle(path.join(__dirname, './src/index.tsx'));
+		const bundled = await bundle(
+			path.join(__dirname, './src/index.tsx'),
+			() => void 0
+		);
 		const comps = await getCompositions(bundled);
 		const video = comps.find((c) => c.id === compositionId);
 		if (!video) {
